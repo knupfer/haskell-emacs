8b0da583e41dbb28c06b55910e2a8c913dc0f769
Florian Knupfer <fknupfer@gmail.com>
2014-12-04 23:58:33 +0100

    async functions and hash-table caching

--- haskell-emacs.el
+++ haskell-emacs.el
@@ -56,27 +56,73 @@ When `haskell-emacs-dir' doesn't exist, it will be created."
   `(progn
      (defun ,(car (read-from-string (file-name-base fun)))
          (&optional string &rest args)
-       ,(concat (file-name-base fun)
-                " is a haskell-function which is started with ARGS
-and feeded with a STRING which is piped to "
-                (if (file-exists-p (concat fun ".hs"))
-                    (concat
-                     "this program:\n\n"
-                     (with-temp-buffer
-                       (insert-file-contents (concat fun ".hs"))
-                       (substring (format "%S" (buffer-string)) 1 -1)))
-                  "a binary."))
+       ,(concat
+         (with-temp-buffer (insert (file-name-base fun)
+                                   " is a haskell-function which
+is started with ARGS and STRING as stdin.  The result is
+considered functional and therefore saved in a hash-tabel to
+speed up repeated calls with the same arguments.  The hash-tabel
+persists an entire emacs session.\n\n")
+                           (let ((fill-column 60))
+                             (fill-region (point-min) (point-max)))
+                           (buffer-string))
+         (when (file-exists-p (concat fun ".hs"))
+           (concat
+            "The source of this function is:\n\n"
+            (with-temp-buffer
+              (insert-file-contents (concat fun ".hs"))
+              (substring (format "%S" (buffer-string)) 1 -1)))))
        (let* ((hash (sxhash (list ,(file-name-base fun) string args)))
               (value (gethash hash haskell-emacs--hash-table)))
          (if value
-             value
+             (eval value)
            (with-temp-buffer
              (when string (insert string))
              (apply (function call-process-region) (point-min) (point-max)
                     ,fun t t nil args)
              (puthash hash (buffer-string) haskell-emacs--hash-table)))))
-     (advice-add ',(car (read-from-string (file-name-base fun))) :before
-                 (lambda (&optional string &rest args) "Haskell function"))))
+     (defun ,(car (read-from-string (concat (file-name-base fun) "-async")))
+         (&optional string &rest args)
+       ,(concat (with-temp-buffer
+                  (insert (file-name-base fun) "-async"
+                          " is a haskell-function which is run
+async with ARGS and STRING as stdin.  The result is considered
+functional and collected in a hash-tabel.  To retrieve the
+result, simply run `" (file-name-base fun) "' with the same
+arguments.  When the result is not already returned, emacs will
+wait for the result, therefor it's always safe to run the base
+command.  Alternatively, you can run eval on the returned
+expression to retrieve the result sync.\n\n")
+                  (let ((fill-column 60))
+                    (fill-region (point-min) (point-max)))
+                  (buffer-string))
+                (when (file-exists-p (concat fun ".hs"))
+                  (concat
+                   "The source of this function is:\n\n"
+                   (with-temp-buffer
+                     (insert-file-contents (concat fun ".hs"))
+                     (substring (format "%S" (buffer-string)) 1 -1)))))
+       (let* ((hash (sxhash (list ,(file-name-base fun) string args)))
+              (value (gethash hash haskell-emacs--hash-table))
+              (process-connection-type nil))
+         (unless value
+           (unless string (setq string ""))
+           (let ((pr (start-process (number-to-string hash) nil ,fun)))
+             (eval `(puthash hash
+                             '(progn (accept-process-output ,pr)
+                                     (gethash ,hash haskell-emacs--hash-table))
+                             haskell-emacs--hash-table))
+             (eval `(apply 'set-process-filter pr
+                           (lambda (proc str)
+                             (puthash ,hash str haskell-emacs--hash-table))
+                           ',args))
+             (process-send-string pr string)
+             (process-send-eof pr)))
+         `(eval (gethash ,hash haskell-emacs--hash-table))))
+     (advice-add ',(car (read-from-string
+                         (concat (file-name-base fun) "-async"))) :before
+                         (lambda (&optional string &rest args)
+                           "Asynchronous Haskell function"))))
 
 (provide 'haskell-emacs)
 
