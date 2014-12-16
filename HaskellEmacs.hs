494d5517c505e2fd90e0158cd9ade93865106948
knupfer <fknupfer@gmail.com>
2014-12-16 16:52:28 +0100

    clean up imports

--- HaskellEmacs.hs
+++ HaskellEmacs.hs
@@ -1,45 +1,39 @@
 {-# LANGUAGE OverloadedStrings #-}
---------------------
 ---- <<import>> ----
---------------------
-import qualified Data.Map
-import qualified Data.Text    as T
-import qualified Data.Text.IO as TIO
-import qualified Data.AttoLisp as AL
-import qualified Data.Attoparsec.ByteString as AP
+import           Data.AttoLisp
+import qualified Data.Attoparsec.ByteString as A (parseOnly)
 import qualified Data.ByteString.Lazy.Char8 as B
-import qualified Data.ByteString.UTF8 as BU
+import qualified Data.ByteString.UTF8       as B (fromString)
+import qualified Data.Map                   as M
+import qualified Data.Text                  as T
+import qualified Data.Text.IO               as T
 
-dispatcher :: Data.Map.Map T.Text (T.Text -> B.ByteString)
-dispatcher = Data.Map.fromList
+dispatcher :: M.Map T.Text (T.Text -> B.ByteString)
+dispatcher = M.fromList
  [
- --------------------
  ---- <<export>> ----
- --------------------
  ]
 
-transform :: (AL.FromLisp a, AL.ToLisp b) => (a -> b) -> T.Text -> B.ByteString
+transform :: (FromLisp a, ToLisp b) => (a -> b) -> T.Text -> B.ByteString
 transform fu str = either (B.pack . (++) "FAIL:")
-                          (failure . fmap (AL.encode . fu) . AL.fromLisp)
-                          . AP.parseOnly AL.lisp . BU.fromString $ T.unpack str
+                          (failure . fmap (encode . fu) . fromLisp)
+                          . A.parseOnly lisp . B.fromString $ T.unpack str
 
-
-failure :: AL.Result B.ByteString -> B.ByteString
-failure (AL.Success a) = B.concat ["DONE:",a]
-failure (AL.Error a ) = B.pack $ "FAIL:" ++ a
-failure _ = B.pack "FAIL:unknown error"
+failure :: Result B.ByteString -> B.ByteString
+failure (Success a) = B.concat ["DONE:",a]
+failure (Error a ) = B.pack $ "FAIL:" ++ a
 
 main :: IO ()
 main = do
-    fun <- TIO.getLine
-    case Data.Map.lookup fun dispatcher of
-      Just function -> TIO.putStrLn "OK:" >> run function
-      _ -> TIO.putStrLn (T.concat ["not found ", fun]) >> main
+    fun <- T.getLine
+    case M.lookup fun dispatcher of
+      Just function -> T.putStrLn "OK:" >> run function
+      _ -> T.putStrLn (T.concat ["not found ", fun]) >> main
 
 run :: (T.Text -> B.ByteString) -> IO ()
 run fun = loop [""]
   where loop xs = do
-         x <- TIO.getLine
+         x <- T.getLine
          if x == "49e3524a756a100a5cf3d27ede74ea95"
             then B.putStrLn (fun . T.unlines $ reverse xs) >> main
             else loop (x:xs)
