ace2c41b3ed2681a8e891271e913df0a8a8bf318
knupfer <fknupfer@gmail.com>
2014-12-16 22:23:22 +0100

    add documentation and make debugging easier

--- HaskellEmacs.hs
+++ HaskellEmacs.hs
@@ -9,34 +9,43 @@ import qualified Data.Text                  as T
 import qualified Data.Text.IO               as T
 import           System.IO                  (hFlush, stdout)
 
+-- | Map of available functions which get transformed to produce and
+-- receive strings.
 dispatcher :: M.Map T.Text (T.Text -> B.ByteString)
 dispatcher = M.fromList
  [
  ---- <<export>> ----
  ]
 
+-- | Transform a curried function to a function which receives and
+-- returns a string in lisp syntax.
 transform :: (FromLisp a, ToLisp b) => (a -> b) -> T.Text -> B.ByteString
-transform fu str = either (B.pack . (++) "=:FAIL:=")
-                          (failure . fmap (encode . fu) . fromLisp)
-                          . A.parseOnly lisp . B.fromString $ T.unpack str
+transform f s = either (B.pack . (++) "=:FAIL:=")
+                       (failure . fmap (encode . f) . fromLisp)
+                       . A.parseOnly lisp . B.fromString $ T.unpack s
 
+-- | Retrieves the contents of the result and annotates whether it was
+-- a success.
 failure :: Result B.ByteString -> B.ByteString
-failure (Success a) = B.concat ["=:PASS:=",a]
-failure (Error a ) = B.pack $ "=:FAIL:=" ++ a
+failure (Success s) = B.concat ["=:PASS:=\n", s]
+failure (Error s)   = B.pack $ "=:FAIL:=\n" ++ s
 
+-- | Lookup functions given in stdin in the dispatcher.
 main :: IO ()
 main = do
-    fun <- T.getLine
-    case M.lookup fun dispatcher of
+    f <- T.getLine
+    case M.lookup f dispatcher of
       Just function -> T.putStrLn "=:OK:=" >> run function
-      _ -> T.putStrLn (T.concat ["=:NULL:=", fun]) >> main
+      _ -> T.putStr (T.unlines $ M.keys dispatcher) >> main
 
+-- | Takes a function and feeds it stdin until all input is given and
+-- prints the output.
 run :: (T.Text -> B.ByteString) -> IO ()
-run fun = loop [""]
+run f = loop [""]
   where loop xs = do
          x <- T.getLine
          if x == "49e3524a756a100a5cf3d27ede74ea95"
-            then B.putStr (fun . T.unlines $ reverse xs)
+            then B.putStrLn (f . T.unlines $ reverse xs)
                  >> B.putStr "=:DONE:="
                  >> hFlush stdout
                  >> main
