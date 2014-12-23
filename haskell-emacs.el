2f27c4a497401f619d97558ddec880422dd1c732
knupfer <fknupfer@gmail.com>
2014-12-23 13:32:10 +0100

    refactor

--- haskell-emacs.el
+++ haskell-emacs.el
@@ -30,122 +30,109 @@
 ;;; Code:
 
 (defvar haskell-emacs-load-dir (file-name-directory load-file-name))
-(defvar haskell-emacs--response nil)
-(defvar haskell-emacs--fun-count 0)
-(defvar haskell-emacs--hash-table (make-hash-table))
-(defvar haskell-emacs-process nil)
 (defvar haskell-emacs-dir "~/.emacs.d/haskell-fun/")
+
+(defvar he/response nil)
+(defvar he/count 0)
+(defvar he/table (make-hash-table))
+(defvar he/proc nil)
+
 (defun haskell-emacs-init ()
   (interactive)
-  (save-excursion
-    (unless (file-directory-p haskell-emacs-dir)
-      (mkdir haskell-emacs-dir t))
-    (let ((funs (directory-files haskell-emacs-dir nil "^[^.].*\.hs$")))
-      (let ((imports)
-            (exports))
-        (setq imports
-              (apply 'concat
-                     (mapcar
-                      (lambda (f)
-                        (let ((ex (haskell-emacs-retrieve-exports f)))
-                          (when ex
-                            (add-to-list 'exports ex)
-                            (concat "import qualified "
-                                    (substring f 0 -3) "\n"))))
-                      funs)))
-        (haskell-emacs-compile
-         (with-temp-buffer
-           (insert-file-contents
-            (concat haskell-emacs-load-dir "HaskellEmacs.hs"))
-           (buffer-string))
-         imports (haskell-emacs-format-exports exports))
-        (when haskell-emacs-process
-          (delete-process haskell-emacs-process))
-        (let ((process-connection-type nil))
-          (setq haskell-emacs-process
-                (start-process "hask" nil
-                               (concat haskell-emacs-dir ".HaskellEmacs"))))
-        (set-process-query-on-exit-flag haskell-emacs-process nil)
-        (set-process-filter
-         haskell-emacs-process
-         (lambda (proc str)
-           (setq haskell-emacs--response
-                 (concat haskell-emacs--response str))
-           (let ((header (read haskell-emacs--response)))
-             (while (<= (+ (car header) (length (format "%s" header)))
-                        (length haskell-emacs--response))
-               (unless (car (last header))
-                 (let ((bug (substring haskell-emacs--response
-                                       (length (format "%s" header))
-                                       (+ (length (format "%s" header)) (car header)))))
-                   (setq haskell-emacs--response (substring haskell-emacs--response
-                                                            (+ (length (format "%s" header))
-                                                               (car header))))
-                   (error bug)))
-               (puthash (cadr header) (substring haskell-emacs--response
-                                                 (length (format "%s" header))
-                                                 (+ (length (format "%s" header)) (car header)))
-                        haskell-emacs--hash-table)
-               (setq haskell-emacs--response (substring haskell-emacs--response
-                                                        (+ (length (format "%s" header))
-                                                           (car header))))
-               (when (> (length haskell-emacs--response) 7)
-                 (setq header (read haskell-emacs--response)))))))
-        (mapc (lambda (fi)
-                (mapc (lambda (fu) (eval (haskell-emacs--wrapper-fun fu)))
-                      fi))
-              exports)))))
+  (unless (file-directory-p haskell-emacs-dir)
+    (mkdir haskell-emacs-dir t))
+  (let ((funs (directory-files haskell-emacs-dir nil "^[^.].*\.hs$")))
+    (let ((imports)
+          (exports))
+      (setq imports (apply 'concat
+                           (mapcar (lambda (f)
+                                     (let ((ex (he/exports-get f)))
+                                       (when ex
+                                         (add-to-list 'exports ex)
+                                         (concat "import qualified "
+                                                 (substring f 0 -3) "\n"))))
+                                   funs)))
+      (he/compile (with-temp-buffer
+                    (insert-file-contents
+                     (concat haskell-emacs-load-dir "HaskellEmacs.hs"))
+                    (buffer-string))
+                  imports (he/exports-format exports))
+      (when he/proc
+        (delete-process he/proc))
+      (let ((process-connection-type nil))
+        (setq he/proc
+              (start-process "hask" nil
+                             (concat haskell-emacs-dir ".HaskellEmacs"))))
+      (set-process-query-on-exit-flag he/proc nil)
+      (set-process-filter he/proc 'he/filter)
+      (mapc (lambda (fi) (mapc (lambda (fu) (eval (he/fun-wrapper fu)))
+                               fi))
+            exports))))
 
-(defun haskell-emacs--fun-body (fun)
-  `(progn
-     (if (stringp ARG)
-         (setq ARG (format "%S" (substring-no-properties ARG)))
-       (if (or (listp ARG) (arrayp ARG))
-           (progn
-             (setq ARG (haskell-emacs--array-to-list ARG))
-             (setq ARG
-                   (concat "("
-                           (apply 'concat
-                                  (mapcar (lambda (x)
-                                            (concat (format "%S" x) "\n"))
-                                          ARG))
-                           ")")))
-         (setq ARG (format "%S" ARG))))
-     (setq haskell-emacs--fun-count (+ 1 haskell-emacs--fun-count))
-     (process-send-string haskell-emacs-process
-                          (concat ,fun " "
-                                  (number-to-string haskell-emacs--fun-count)
-                                  " " (number-to-string
-                                       (with-temp-buffer (insert ARG)
-                                                         (let ((lines 1))
-                                                           (goto-char (point-min))
-                                                           (while (re-search-forward "\n" nil t)
-                                                             (setq lines (+ lines 1)))
-                                                           lines)))
-                                  "\n" ARG "\n"))))
+(defun he/filter (proc str)
+  (setq he/response (concat he/response str))
+  (let ((header (read he/response)))
+    (while (<= (+ (car header) (length (format "%s" header)))
+               (length he/response))
+      (unless (car (last header))
+        (let ((bug (substring he/response
+                              (length (format "%s" header))
+                              (+ (length (format "%s" header)) (car header)))))
+          (setq he/response (substring he/response
+                                       (+ (length (format "%s" header))
+                                          (car header))))
+          (error bug)))
+      (puthash (cadr header)
+               (substring he/response
+                          (length (format "%s" header))
+                          (+ (length (format "%s" header)) (car header)))
+               he/table)
+      (setq he/response (substring he/response (+ (length (format "%s" header))
+                                                  (car header))))
+      (when (> (length he/response) 7)
+        (setq header (read he/response))))))
 
-(defun haskell-emacs--wrapper-fun (fun)
+(defun he/fun-body (fun)
+  (setq ARG
+        (if (stringp ARG)
+            (format "%S" (substring-no-properties ARG))
+          (if (or (listp ARG) (arrayp ARG))
+              (concat "("
+                      (apply 'concat (mapcar (lambda (x)
+                                               (concat (format "%S" x) "\n"))
+                                             (he/array-to-list ARG))) ")")
+            (format "%S" ARG))))
+  (setq he/count (+ 1 he/count))
+  (process-send-string
+   he/proc (concat fun " " (number-to-string he/count) " "
+                   (number-to-string
+                    (with-temp-buffer (insert ARG)
+                                      (let ((lines 1))
+                                        (goto-char (point-min))
+                                        (while (re-search-forward "\n" nil t)
+                                          (setq lines (+ lines 1)))
+                                        lines)))
+                   "\n" ARG "\n")))
+
+(defun he/fun-wrapper (fun)
   "Take FUN and return a wrapper in elisp."
-  (let ((body (haskell-emacs--fun-body fun)))
-    `(progn
-       (byte-compile (defun ,(intern fun) (ARG) ,body
-                            (haskell-emacs--get-result haskell-emacs--fun-count)))
-       (byte-compile (defun ,(intern (concat fun "-async")) (ARG) ,body
-                            `(haskell-emacs--get-result ,haskell-emacs--fun-count))))))
+  (let ((body `(he/fun-body ,fun)))
+    `(progn (byte-compile (defun ,(intern fun) (ARG) ,body
+                                 (he/get he/count)))
+            (byte-compile (defun ,(intern (concat fun "-async")) (ARG) ,body
+                                 `(he/get ,he/count))))))
 
-(defun haskell-emacs--get-result (key)
-  (while (not (gethash key haskell-emacs--hash-table))
-    (accept-process-output haskell-emacs-process))
-  (read (gethash key haskell-emacs--hash-table)))
+(defun he/get (key)
+  (while (not (gethash key he/table))
+    (accept-process-output he/proc))
+  (read (gethash key he/table)))
 
-(defun haskell-emacs--array-to-list (a)
-  (mapcar (lambda (x) (if (and (not (stringp x))
-                               (or (arrayp x) (listp x)))
-                          (haskell-emacs--array-to-list x)
-                        x))
+(defun he/array-to-list (a)
+  (mapcar (lambda (x) (if (and (not (stringp x)) (or (arrayp x) (listp x)))
+                          (he/array-to-list x) x))
           a))
 
-(defun haskell-emacs-format-exports (l)
+(defun he/exports-format (l)
   (let ((result
          (apply 'concat
                 (mapcar
@@ -153,7 +140,7 @@
                    (if x
                        (apply 'concat
                               (mapcar
-                               (lambda (y) (concat "(\""y "\",transform "y "),"))
+                               (lambda (y) (concat "(\""y"\",transform "y"),"))
                                x))
                      ""))
                  l))))
@@ -161,38 +148,42 @@
         (substring result 0 -1)
       "")))
 
-(defun haskell-emacs-retrieve-exports (file-name)
-  (let ((f-base (file-name-base file-name)))
+(defun he/exports-get (file-name)
+  (let ((f-base (file-name-base file-name))
+        (w "[ \t\n\r]"))
     (with-temp-buffer
       (insert-file-contents (concat haskell-emacs-dir file-name))
       (when (re-search-forward
-             (concat "^module[ \t\n\r]+" f-base
-                     "[ \t\n\r]*([ \n\t\r]*\\(\\(?:[^)]\\|[ \n\t\r]\\)+\\)[ \n\t\r]*)")
+             (concat "^module" w "+" f-base
+                     w "*(" w "*\\(\\(?:[^)]\\|" w "\\)+\\)" w "*)")
              nil t)
-        (mapcar (lambda (fu) (concat f-base "." (car (last (split-string fu "\\.")))))
+        (mapcar (lambda (fu) (concat f-base "."
+                                     (car (last (split-string fu "\\.")))))
                 (split-string (match-string 1) "[ \n\r,]+"))))))
 
-(defun haskell-emacs-compile (code import export)
+(defun he/compile (code import export)
   (with-temp-buffer
-    (insert code)
-    (goto-char (point-min))
-    (when (re-search-forward "---- <<import>> ----" nil t)
-      (replace-match import))
-    (when (re-search-forward "---- <<export>> ----" nil t)
-      (replace-match export))
-    (cd haskell-emacs-dir)
-    (unless (and (file-exists-p ".HaskellEmacs.hs")
-                 (equal (buffer-string)
-                        (with-temp-buffer
-                          (insert-file-contents ".HaskellEmacs.hs")
-                          (buffer-string))))
-      (write-file ".HaskellEmacs.hs"))
-    (unless (equal 0 (call-process "ghc" nil "*HASKELL-EMACS*"
-                                   nil "-O2" "--make" ".HaskellEmacs.hs"))
-      (let ((bug (with-current-buffer "*HASKELL-EMACS*" (buffer-string))))
-        (kill-buffer "*HASKELL-EMACS*")
-        (error bug)))
-    (kill-buffer "*HASKELL-EMACS*")))
+    (let ((heF ".HaskellEmacs.hs")
+          (heB "*HASKELL-BUFFER*"))
+      (insert code)
+      (goto-char (point-min))
+      (when (re-search-forward "---- <<import>> ----" nil t)
+        (replace-match import))
+      (when (re-search-forward "---- <<export>> ----" nil t)
+        (replace-match export))
+      (cd haskell-emacs-dir)
+      (unless (and (file-exists-p heF)
+                   (equal (buffer-string)
+                          (with-temp-buffer
+                            (insert-file-contents heF)
+                            (buffer-string))))
+        (write-file heF))
+      (unless (equal 0 (call-process "ghc" nil heB
+                                     nil "-O2" "--make" heF))
+        (let ((bug (with-current-buffer heB (buffer-string))))
+          (kill-buffer heB)
+          (error bug)))
+      (kill-buffer heB))))
 
 (provide 'haskell-emacs)
 
