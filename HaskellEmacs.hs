14bb8fb258eb6952267a71639005eb403737bc07
knupfer <fknupfer@gmail.com>
2014-12-27 18:41:56 +0100

    small refactoring

--- HaskellEmacs.hs
+++ HaskellEmacs.hs
@@ -7,27 +7,28 @@ import           Control.Parallel.Strategies (rdeepseq, using)
 import           Data.AttoLisp
 import           Data.Attoparsec.ByteString  (parseOnly)
 import qualified Data.ByteString.Lazy.Char8  as B
-import qualified Data.ByteString.UTF8        as B (fromString, toString)
 import qualified Data.Map                    as M
 import           Data.Maybe                  (fromJust)
 import           Data.Monoid                 ((<>))
+import           Data.Text                   (Text)
 import qualified Data.Text                   as T
+import           Data.Text.Encoding
 import qualified Data.Text.IO                as T
 import           System.IO                   (hFlush, stdout)
+import qualified Text.Show.Text              as T (show)
 
--- | Lookup functions given in stdin in the dispatcher.
+-- | Watch for commands and dispatch them in a seperate fork.
 main :: IO ()
 main = do printer <- newEmptyMVar
-          forkIO . forever $ takeMVar printer >>= B.putStrLn >> hFlush stdout
+          forkIO . forever $ takeMVar printer >>= T.putStrLn >> hFlush stdout
           forever $
             do (f,n,line) <- extract <$> T.getLine
-               xs         <- replicateM line T.getLine
-               let r = run f n xs
-               forkIO $ (r `using` rdeepseq) `seq` putMVar printer r
+               result     <- run f n <$> replicateM line T.getLine
+               forkIO $ (result `using` rdeepseq) `seq` putMVar printer result
 
 -- | Map of available functions which get transformed to produce and
 -- receive strings.
-dispatcher :: M.Map T.Text (T.Text -> B.ByteString)
+dispatcher :: M.Map Text (Text -> Text)
 dispatcher = M.fromList
  [
  ---- <<export>> ----
@@ -35,30 +36,22 @@ dispatcher = M.fromList
 
 -- | Transform a curried function to a function which receives and
 -- returns a string in lisp syntax.
-transform :: (FromLisp a, ToLisp b) => (a -> b) -> T.Text -> B.ByteString
-transform f = either (B.pack . (++) " nil)")
-                      (failure . fmap (encode . f) . fromLisp)
-                      . parseOnly lisp . B.fromString . T.unpack
+transform :: (FromLisp a, ToLisp b) => (a -> b) -> Text -> Text
+transform f = either (T.pack . (++) " nil)") toText . parseOnly lisp . encodeUtf8
+  where toText = failure . fmap (decodeUtf8 . B.toStrict . encode . f) . fromLisp
 
 -- | Retrieves the contents of the result and annotates whether it was
 -- a success.
-failure :: Result B.ByteString -> B.ByteString
+failure :: Result Text -> Text
 failure (Success s) = " yes)" <> s
-failure (Error s)   = B.pack $ " nil)" ++ s
+failure (Error s)   = T.pack $ " nil)" ++ s
 
-extract :: T.Text -> (T.Text, T.Text, Int)
+extract :: Text -> (Text, Text, Int)
 extract = (\(x:y:z:_) -> (x, y, read $ T.unpack z)) . T.words
 
 -- | Takes a function and feeds it stdin until all input is given and
 -- prints the output.
-run :: T.Text -> T.Text -> [T.Text] -> B.ByteString
-run f n xs = B.concat
-               [ "("
-               , B.pack . show . length
-                               . dropWhile (/=')')
-                               . B.toString $ B.toStrict result
-               , " "
-               , B.pack $ T.unpack n
-               , result
-               ]
-        where result = fromJust (M.lookup f dispatcher) $ T.unlines xs
+run :: Text -> Text -> [Text] -> Text
+run f resultId xs = T.concat ["(", msgLength, " ", resultId, result]
+  where result    = fromJust (M.lookup f dispatcher) $ T.unlines xs
+        msgLength = T.show . T.length $ T.dropWhile (/= ')') result
