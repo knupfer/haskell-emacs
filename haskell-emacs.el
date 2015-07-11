2df75bf9e02cdf9167e7ffd4506866d04760690d
knupfer <fknupfer@gmail.com>
2015-07-11 23:04:16 +0200

    update documentation
    Reflect the new installation dialog and functions which take or return
    lisps.

--- haskell-emacs.el
+++ haskell-emacs.el
@@ -112,19 +112,35 @@ asynchronous elisp function.
 
 Dependencies:
  - GHC
- - attoparsec
+ - cabal
+
+Haskell packages:
  - atto-lisp
+ - parallel
+ - utf8-string
 
 Consider that you've got the following toy program:
 
 ---- ~/.emacs.d/haskell-fun/Matrix.hs
-module Matrix (transpose, dyadic) where
+module Matrix (identity,isIdentity,dyadic,transpose) where
 
 import qualified Data.List as L
 
+-- | Takes a matrix (a list of lists of ints) and returns its transposition.
 transpose :: [[Int]] -> [[Int]]
 transpose = L.transpose
 
+-- | Returns an identity matrix of size n.
+identity :: Int -> [[Int]]
+identity n
+  | n > 1 = L.nub $ L.permutations $ 1 : replicate (n-1) 0
+  | otherwise = [[1]]
+
+-- | Check whether a given matrix is a identity matrix.
+isIdentity :: [[Int]] -> Bool
+isIdentity xs = xs == identity (length xs)
+
+-- | Compute the dyadic product of two vectors.
 dyadic :: [Int] -> [Int] -> [[Int]]
 dyadic xs ys = map (\\x -> map (x*) ys) xs
 ----
@@ -202,13 +218,25 @@ modularity and using haskell for even more basic tasks."
                       (error (buffer-string)))))
                 (if example
                     (with-temp-buffer (insert "
-module Matrix (transpose, dyadic) where
+module Matrix (identity,isIdentity,dyadic,transpose) where
 
 import qualified Data.List as L
 
+-- | Takes a matrix (a list of lists of ints) and returns its transposition.
 transpose :: [[Int]] -> [[Int]]
 transpose = L.transpose
 
+-- | Returns an identity matrix of size n.
+identity :: Int -> [[Int]]
+identity n
+  | n > 1 = L.nub $ L.permutations $ 1 : replicate (n-1) 0
+  | otherwise = [[1]]
+
+-- | Check whether a given matrix is a identity matrix.
+isIdentity :: [[Int]] -> Bool
+isIdentity xs = xs == identity (length xs)
+
+-- | Compute the dyadic product of two vectors.
 dyadic :: [Int] -> [Int] -> [[Int]]
 dyadic xs ys = map (\\x -> map (x*) ys) xs")
                                       (write-file (concat haskell-emacs-dir
