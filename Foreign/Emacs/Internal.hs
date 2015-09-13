33c06121edbf561f0c1d3b9aef71c03e6f6309ac
knupfer <fknupfer@gmail.com>
2015-09-13 14:58:21 +0200

    implement Foreign.Emacs.Internal.eval

--- Foreign/Emacs/Internal.hs
+++ Foreign/Emacs/Internal.hs
@@ -1,9 +1,27 @@
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE OverlappingInstances      #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE UndecidableInstances      #-}
+
 module Foreign.Emacs.Internal where
 
 import           Control.Concurrent
+import           Control.DeepSeq
+import           Control.Monad.Trans
 import           Control.Monad.Trans.Reader
 import           Data.AttoLisp
-import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.ByteString.Lazy.Char8 as B hiding (length)
+import qualified Data.ByteString.Lazy.UTF8  as B (length)
+import           Data.Monoid                ((<>))
+
+class ToEmacs a where
+  toEmacs :: a -> Either (Emacs Lisp) Lisp
+
+instance ToLisp a => ToEmacs a where
+  toEmacs = Right . toLisp
+
+instance ToLisp a => ToEmacs (Emacs a) where
+  toEmacs em = Left $ fmap toLisp em
 
 newtype Emacs a = EmacsInternal
   {fromEmacs :: ReaderT (MVar Lisp, Chan B.ByteString) IO a}
@@ -17,3 +35,25 @@ instance Applicative Emacs where
 
 instance Monad Emacs where
   (EmacsInternal x) >>= f = EmacsInternal $ x >>= fromEmacs . f
+
+instance NFData (Emacs Lisp) where
+  rnf (EmacsInternal _) = ()
+
+eval :: (ToLisp a, FromLisp a) => [Lisp] -> Emacs a
+eval lsp = EmacsInternal $ do
+  (mvar, chan) <- ask
+  liftIO $ writeChan chan cmd
+  List (a:_) <- liftIO $ takeMVar mvar
+  case fromLisp a of
+       Success b -> return b
+       Error msg -> error msg
+  where cmd = let x = encode $ List [ Symbol "process-send-string"
+                                    , Symbol "haskell-emacs--proc"
+                                    , List [ Symbol "format"
+                                           , String "|%S"
+                                           , List [ Symbol "list"
+                                                  , List lsp ]]]
+              in encode [B.length x] <> x
+
+eval_ :: [Lisp] -> Emacs ()
+eval_ = fmap (const () :: Lisp -> ()) . eval
